de366d7971cd4170c8943b72284b1ab2adf7c8bd
Anna-Nikolaeva <anna88nikolaeva@gmail.com>
2016-04-17 15:27:41 +0300

    Added two more tests

--- sandbox/src/test/java/jft/sandbox/PointTests.java
+++ sandbox/src/test/java/jft/sandbox/PointTests.java
@@ -16,5 +16,12 @@ public class PointTests {
         Point p2 = new Point(3.0, -12.0);
         Assert.assertEquals(p1.calculateDistance(p2), 20.0);
 
+        Point p3 = new Point(-45.0, 8.0);
+        Assert.assertEquals(p3.calculateDistance(p1), 48.0);
+
+        Point p4 = new Point(23.56, -123.62);
+        Double distance = (double)Math.round(p2.calculateDistance(p4) * 100d) / 100d;
+        Assert.assertEquals(distance, 113.5);
+
     }
 }
