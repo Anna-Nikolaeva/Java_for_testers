71bbb1356522ade173ffefed403d57b868b345b9
Anna-Nikolaeva <anna88nikolaeva@gmail.com>
2016-05-22 18:41:00 +0300

    Addded possibility to insert data from file for groupd creation

--- addressbook-web-tests/src/test/java/jft/addressbook/tests/GroupCreator.java
+++ addressbook-web-tests/src/test/java/jft/addressbook/tests/GroupCreator.java
@@ -2,8 +2,13 @@ package jft.addressbook.tests;
 
 import jft.addressbook.model.GroupData;
 import jft.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -11,17 +16,30 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreator extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.scv")));
+        String line = reader.readLine();
+        while(line != null){
+            String[] split = line.split(";");
+            list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
 
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData newGroup = new GroupData().withName("test1");
-        app.group().create(newGroup);
+        //GroupData newGroup = new GroupData().withName("test1");
+        app.group().create(group);
         assertThat(app.group().getGroupCount(), equalTo(before.size() + 1));
         Set<GroupData> after = app.group().all();
         assertThat(after, equalTo(
-                before.withAdded(newGroup.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+                before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
     }
 
     @Test
