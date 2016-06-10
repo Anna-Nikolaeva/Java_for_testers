c0d9e28e16fa1fa13679eca48e47214e5272fdeb
Anna-Nikolaeva <anna88nikolaeva@gmail.com>
2016-06-10 16:22:05 +0300

    New module for Mattis test was added

---
+++ mantis-tests/src/test/java/jft/mantis/tests/TestBase.java
@@ -0,0 +1,25 @@
+package jft.mantis.tests;
+
+import jft.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * Created by Anna on 17.04.16.
+ */
+public class TestBase {
+
+    protected static final ApplicationManager app =
+            new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite (alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+}
