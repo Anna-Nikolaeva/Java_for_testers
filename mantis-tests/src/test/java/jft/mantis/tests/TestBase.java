2e39b2a01f0a496151d938d34d5eafc77c89cf34
Anna-Nikolaeva <anna88nikolaeva@gmail.com>
2016-06-14 01:05:39 +0300

    Added possibility to check before test whether issue was fixed

--- mantis-tests/src/test/java/jft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/jft/mantis/tests/TestBase.java
@@ -2,8 +2,13 @@ package jft.mantis.tests;
 
 import jft.mantis.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import javax.xml.rpc.ServiceException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 /**
  * Created by Anna on 17.04.16.
@@ -21,5 +26,12 @@ public class TestBase {
     @AfterSuite (alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+
+    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (app.soap().isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
     }
 }
